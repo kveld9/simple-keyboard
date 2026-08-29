f1462dd19fbe29cd84d2e5ce2ea1b8419ea01ea3
soyelmismo <thinkboutme@protonmail.com>
2026-08-28 19:30:10 -0500

    Fix settings constants merge

--- app/src/main/java/rkr/simplekeyboard/inputmethod/latin/settings/Settings.java
+++ app/src/main/java/rkr/simplekeyboard/inputmethod/latin/settings/Settings.java
@@ -65,15 +65,12 @@ public final class Settings extends BroadcastReceiver implements SharedPreferenc
     public static final String PREF_SPACE_SWIPE = "pref_space_swipe";
     public static final String PREF_DELETE_SWIPE = "pref_delete_swipe";
     public static final String PREF_CLIPBOARD_RETENTION_TIME = "pref_clipboard_retention_time";
-<<<<<<< HEAD
     public static final String PREF_KEY_SHAPE = "pref_key_shape";
     public static final String KEY_SHAPE_ROUNDED = "rounded";
     public static final String KEY_SHAPE_RECTANGULAR = "rectangular";
     public static final String KEY_SHAPE_BORDERLESS = "borderless";
-=======
     public static final String PREF_SHOW_SUGGESTIONS = "pref_show_suggestions";
     public static final String PREF_AUTO_CORRECTION = "pref_auto_correction";
->>>>>>> master
 
     private static final float UNDEFINED_PREFERENCE_VALUE_FLOAT = -1.0f;
     private static final int UNDEFINED_PREFERENCE_VALUE_INT = -1;
